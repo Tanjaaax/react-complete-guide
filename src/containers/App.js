9690090458337d99c5cd027fe84c0956f54a21fe
Tanja van de Merbel <t.van.de.merbel@hilverdadeboer.nl>
2019-04-09 15:38:49 +0200

    101: Higher Order Components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 import classes from './App.css';
 
@@ -75,7 +76,7 @@ class App extends Component {
 
     if (this.state.showPersons) {
       persons = (
-          <Persons 
+          <Persons
             persons={this.state.persons}
             clicked={this.deletePersonHandler}
             changed={this.nameChangedHandler}
@@ -84,23 +85,23 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
-      <button 
-        onClick={() => {
-          this.setState({ showCockpit: false })
-        }}
-      > Remove Cockpit
-      </button>
+      <WithClass classes={classes.App}>
+        <button
+          onClick={() => {
+            this.setState({ showCockpit: false })
+          }}
+        > Remove Cockpit
+        </button>
 
-      {this.state.showCockpit ? (
-        <Cockpit 
-          showPersons={this.state.showPersons}
-          personsLength={this.state.persons.length}
-          clicked={this.togglePersonsHandler}
-        />
-      ) : null }
-        {persons}
-      </div>
+        {this.state.showCockpit ? (
+          <Cockpit
+            showPersons={this.state.showPersons}
+            personsLength={this.state.persons.length}
+            clicked={this.togglePersonsHandler}
+          />
+        ) : null }
+          {persons}
+      </WithClass>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
   }
